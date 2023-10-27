35deec19f4a111c5daf1843bb8df88fb5fbdfc2f
Filip Žitný <filipzitny@gmail.com>
2023-10-28 00:23:14 +0200

    init

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(name)s %(levelnam
 
 window = tk.Tk()
 window.title("Skynet")
-window.geometry("500x500")
+window.geometry("400x200")
 
 hack = tk.PhotoImage(file="68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f4e41355337463264696b4144752f67697068792e676966.gif")
 bg_hack = tk.Label(window, image=hack)
+bg_hack.place(x=0, y=0, relwidth=1, relheight=1)
+
+# hack_label = tk.Label(window, text="Hacking into the mainframe...", bg="black")
+# hack_label.pack(pady=20)
+
 
 if __name__ == '__main__':
     while True:
